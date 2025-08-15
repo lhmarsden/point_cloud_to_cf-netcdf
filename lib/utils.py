76f8af60c712e65138207a4df59420d2a6725e4c
Luke Marsden <lukem@met.no>
2025-08-15 14:56:04 +0200

    checking time against iso

--- lib/utils.py
+++ lib/utils.py
@@ -2,13 +2,16 @@ import re
 import spectral as sp
 import logging
 import numpy as np
+from dateutil.parser import isoparse
 
 logger = logging.getLogger(__name__)
 
 def validate_time_format(time_string):
-    # Regular expression to match the format YYYY-MM-DD or YYYY-MM-DDTHH:MM:SSZ
-    pattern = r'^\d{4}-\d{2}-\d{2}(T\d{2}:\d{2}:\d{2}Z)?$'
-    return bool(re.match(pattern, time_string))
+    try:
+        isoparse(time_string)
+        return True
+    except ValueError:
+        return False
 
 def define_chunk_size(pc_df, hdr_filepath):
     errors = []
